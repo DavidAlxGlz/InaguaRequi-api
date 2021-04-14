f13d0b7db45d7c81562633f43fa330cbdb544ceb
David Alejandro <david.alx.glz@gmail.com>
2021-04-14 14:15:27 -0500

    14/04/2021 antes del cambio de carpetas

--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,6 @@ import { createPool } from "mysql2/promise";
 export async function connect(){
 
    const connection = await createPool({
-      
         host: 'localhost',
         user: 'root',
         password: '2602754',
