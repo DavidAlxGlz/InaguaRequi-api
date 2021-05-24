b6ed290f5cd42a65f78cf780e6d748fd42004dcb
David <david.alx.glz@gmail.com>
2021-05-24 00:33:47 -0500

    24/05/2021 proveedores search

--- src/database.ts
+++ src/database.ts
@@ -5,7 +5,7 @@ export async function connect(){
    const connection = await createPool({
         host: 'localhost',
         user: 'root',
-        password: '2602754',
+        password: 'Inagua2008',
         database: 'inagua_requis',
        
     });
